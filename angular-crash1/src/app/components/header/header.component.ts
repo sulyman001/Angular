ff7e2c51b84b929792c21b8cd868c63099c73113
Sulyman <sulymansulyman001@gmail.com>
2023-01-30 19:24:49 +0100

    Updated the document

--- angular-crash1/src/app/components/header/header.component.ts
+++ angular-crash1/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,16 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription = new Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask();
   }
 }
