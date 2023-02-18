740fbe77c9c01a2bd5ecb5af393ccbcc53391a0d
Sulyman <sulymansulyman001@gmail.com>
2023-02-19 00:10:56 +0100

    Adding new project

--- userVerification-angular/src/app/app.module.ts
+++ userVerification-angular/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment.development';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ userVerification-angular/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+
+  production: false,
+
+  firebase: {
+    apiKey: "AIzaSyAallYNbbuUqL4jtsKvttW56mMWCO6VCSk",
+    authDomain: "testing-131d2.firebaseapp.com",
+    projectId: "testing-131d2",
+    storageBucket: "testing-131d2.appspot.com",
+    messagingSenderId: "350442324801",
+    appId: "1:350442324801:web:f3efac8e36144764db97c0"
+  }
+};
