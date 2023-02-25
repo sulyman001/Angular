1566b3c1ab30c00dae378a853bd8b1fad388e82d
Sulyman <sulymansulyman001@gmail.com>
2023-02-25 21:05:04 +0100

    updated model & dashboard

--- userVerification-angular/src/app/component/dashboard/dashboard.component.ts
+++ userVerification-angular/src/app/component/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Student } from 'src/app/model/student';
 import { AuthService } from 'src/app/shared/auth.service';
+import { DataService } from 'src/app/shared/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +10,44 @@ import { AuthService } from 'src/app/shared/auth.service';
 })
 export class DashboardComponent {
 
-  constructor(private auth : AuthService) { }
+  studentsList : Student[] = [];
+  id : string = '';
+  first_name : string = '';
+  last_name : string = '';
+  email : string = '';
+  mobile : string = '';
+
+  constructor(private auth : AuthService, private data : DataService) { }
 
   register(){
     this.auth.logout();
+  }
+
+  getAllStudents() {
+    this.data.getAllStudents().subscribe(res => {
+
+      this.studentsList = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        data.id = e.payload.doc.id;
+        return data;
+      })
+
+    }, err => {
+      alert('Error while fetching student data');
+    })
+  }
+
+  addStudent() {
+
+  }
+
+  updateStudent() {
+
+  }
+
+  deleteStudent(student : Student){
+    if(window.confirm('Are you sure you want to delete'+student.first_name+''+student.last_name+'')){
+      this.data.deleteStudent(student);
+    }
   }
 }
---
+++ userVerification-angular/src/app/shared/data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Student } from '../model/student';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private afs : AngularFirestore) { }
+
+  // add student
+  addStudent(student : Student) {
+    student.id = this.afs.createId();
+    return this.afs.collection('/students').add(student);
+  }
+
+  // get all students
+  getAllStudents() {
+    return this.afs.collection('/Students').snapshotChanges();
+  }
+
+  // delete student
+  deleteStudent(student : Student){
+    return this.afs.doc('/Students/'+student.id).delete();
+  }
+
+  // Update student
+  updateStudent(student : Student){
+    this.deleteStudent(student);
+    this.addStudent(student);
+  }
+}
